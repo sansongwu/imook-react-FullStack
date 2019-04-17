40057860060688c2e72c9977f9522a789060c32f
wayne <576377580@qq.com>
2019-04-18 01:37:46 +0800

    增加了cnode接口的代理 和 login接口

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const path = require('path')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
 const favicon = require('serve-favicon')
 const express = require('express')
@@ -10,7 +12,22 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const app = express()
 
+app.use(bodyParser.json()) // 在application的数据挂载到request.body上
+app.use(bodyParser.urlencoded({ extended: false })) // 其他请求方式的数据也挂载到request.body上？
+
+app.use(session({
+  maxAge: 10 * 60 * 1000,
+  name: 'tid', // session 会放一个cocki id 放在浏览器端
+  resave: false,
+  saveUninitialized: false,
+  secret: 'react cnode' // 用该字符串加密coocki
+}))
+
 app.use(favicon(path.join(__dirname, '../favicon.ico'))) // 设置favicon的请求地址
+
+/* 拦截请求 */
+app.use('/api/user', require('./util/handle-login'))
+app.use('/api', require('./util/proxy'))
 
 if (!isDev) {
     const serverEntry = require('../dist/server_entry').default // 服务端代码在 serverEntry 里    .default见简书3-7
